14790f6804ad6983315cf3ac9444b136b3d1e8ac
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2023-11-13 16:53:47 +0530

    Add: movie model created

---
+++ src/main/java/com/example/movieapi/movie.java
@@ -0,0 +1,26 @@
+package com.example.movieapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class movie {
+    @Id
+    private ObjectId id;
+    private String imdbid;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> generes;
+    private List<String> brackdrops;
+}
